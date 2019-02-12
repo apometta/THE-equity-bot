258ffa231c50f7d881421a9e9c44c5d8741d7a12
apometta <apomet@gmail.com>
2019-02-11 21:40:58 -0800

    Moved/renamed files.

--- source/db_handler.py
+++
@@ -1,4 +0,0 @@
-"""This file, written by Andrew H. Pometta, is responsible for interfacing
-with the database for THE-equity-bot.  It is used by the main program to
-handle all interactions with te database, or standalone to access, maintain,
-and read from the database."""
--- source/exec_handler.py
+++
@@ -1,4 +0,0 @@
-"""This file, written by Andrew H. Pometta, is the file responsible for
-interfacing with the equity analysis executable.  It is used by the main
-Python file for this program, reddit_handler.py, to go between the executable
-and the rest of the program."""
