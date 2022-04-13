cfa931988eb6826f737d1a75d12383feaec0f645
Andrew H. Pometta <apomet@gmail.com>
2022-04-12 22:25:07 -0700

    Initial commit.

---
+++ source/main.py
@@ -0,0 +1,6 @@
+"""
+This file, written by Andrew H. Pometta, is the main source file for the 
+equity_bot Reddit bot.  See README.md for more details.
+
+Some of these tasks might be diverted into different files later.
+"""
