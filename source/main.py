a9b51176c7dfa7df3851d10d61aac1b4c002fcc5
Andrew H. Pometta <apomet@gmail.com>
2023-08-24 14:28:45 -0700

    Added Base mention checking

--- source/main.py
+++ source/main.py
@@ -4,3 +4,21 @@ equity_bot Reddit bot.  See README.md for more details.
 
 Some of these tasks might be diverted into different files later.
 """
+
+import praw
+import os
+
+reddit = praw.Reddit(
+    "equitybot", user_agent="script:equitybot:v0.2 (Andrew H. Pometta)")
+
+
+def perform_some_action():
+    sleep(5)
+
+done = False
+while not done:
+    perform_some_action()
+    for i in reddit.inbox.mentions(limit=10):
+        if not i.new:
+            break
+        i.mark_read()
